b2e584d8dd157aac8e4f0ea74735a373fdd6d006
jeromeklam <jeromeklam@free.fr>
2020-12-30 15:31:49 +0100

    First commit

---
+++ src/main/java/org/freeasso/myhellojib/HelloController.java
@@ -0,0 +1,24 @@
+package org.freeasso.myhellojib;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        StringBuilder message = new StringBuilder("Hello Jib Maven Plugin!");
+        try {
+            InetAddress ip = InetAddress.getLocalHost();
+            message.append(" From host: " + ip);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return message.toString();
+    }
+
+}
